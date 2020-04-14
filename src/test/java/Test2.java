db09b2485095d2122774fb0a51357ac4f9683e18
qiangdabao <983859927@qq.com>
2020-04-14 16:07:44 +0800

    提交test2-1

--- src/test/java/Test2.java
+++ src/test/java/Test2.java
@@ -16,12 +16,12 @@ import java.util.Date;
 
 
 public class Test2 {
-    
-
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("11111111111111");
-		
+        System.out.println("11111111111111-1");
+        System.out.println("11111111111111-2");
+        System.out.println("11111111111111-3");
+        System.out.println("11111111111111-4");
 	}
-
 }
