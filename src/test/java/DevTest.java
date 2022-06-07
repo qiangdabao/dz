ec4744c0d57f530e6a4bd43a30fa658b2e0260fa
qiangdabao <983859927@qq.com>
2022-06-07 21:53:41 +0800

    devTest 新建

---
+++ src/test/java/DevTest.java
@@ -0,0 +1,9 @@
+/**
+ * @author qiangdabao
+ * @date 2022/6/7 21:43
+ */
+public class DevTest {
+    public static void main (String args[]) {
+        System.out.println("DevTest is starting!!");
+    }
+}
