c167223fa9fbec7f22652fd1552c643c3acc71cb
qiangdabao <983859927@qq.com>
2022-06-08 19:28:05 +0800

    Test5 dev 新建

--- src/test/java/Test5.java
+++ src/test/java/Test5.java
@@ -1,5 +1,9 @@
-package PACKAGE_NAME;/**
-* @author qiangdabao 
-* @date 2022/6/8 17:45
-*/public class Test5 {
+/**
+ * @author qiangdabao
+ * @date 2022/6/8 17:45
+ */
+public class Test5 {
+    public static  void main (String args[]) {
+        System.out.println("1234567890");
+    }
 }
